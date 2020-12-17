4c92aeee50f7f6d8db5fc52030d5ca005f125093
Cecil Thomas <cejohanner@gmail.com>
2020-12-17 15:21:43 -0400

    splits arr in half

--- Ruby/recursion/spec/merge_sort_spec.rb
+++ Ruby/recursion/spec/merge_sort_spec.rb
@@ -3,31 +3,13 @@
 require './lib/merge_sort'
 
 describe MergeSort do
-  xcontext 'not using recursion' do
-    it "returns if arr's length is less than or equal to 1" do
-      expect(subject.merge_sort([1])).to eql([1])
-    end
-
-    it 'splits arr in half' do
-      expect(subject.merge_sort([2, 1])).to eql([[2], [1]])
-    end
-
-    it 'splits arr in half' do
-      expect(subject.merge_sort([4, 3, 2, 1])).to eql([[4, 3], [2, 1]])
-    end
-  end
-
   context 'using recursion' do
-    it "returns if arr's length is less than or equal to 1" do
+    it 'returns if arr.length is less than 2' do
       expect(subject.merge_sort([1])).to eql([1])
     end
 
-    it 'splits arr in half' do
+    it 'splits arr in halfs' do
       expect(subject.merge_sort([2, 1])).to eql([[2], [1]])
-    end
-
-    it 'splits arr in half' do
-      expect(subject.merge_sort([4, 3, 2, 1])).to eql([[[4], [3]], [[2], [1]]])
     end
   end
 end
